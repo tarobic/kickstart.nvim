193b0b6848faed0aea709314003a449133f36c9f
tarobic <msullentrup1@gmail.com>
2025-05-05 12:20:03 -0500

    Move init.lua options to separate file

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,6 @@
 vim.g.have_nerd_font = true
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
 -- [[ Setting options ]]
 vim.opt.number = true
@@ -19,16 +19,35 @@ vim.opt.confirm = true
 vim.opt.showmode = false
 vim.opt.winborder = 'rounded'
 
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
 vim.schedule(function()
-   vim.opt.clipboard = 'unnamedplus'
+  vim.opt.clipboard = 'unnamedplus'
 end)
 
--- [[ Keymaps ]]
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.cmd 'set expandtab'
+vim.cmd 'set tabstop=3'
+vim.cmd 'set softtabstop=3'
+vim.cmd 'set shiftwidth=3'
 
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=3")
-vim.cmd("set softtabstop=3")
-vim.cmd("set shiftwidth=3")
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
 
-vim.cmd.colorscheme = "catppuccin"
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
