421df2c43916426ac811a72b3138802f37c402ef
tarobic <msullentrup1@gmail.com>
2025-05-06 22:23:27 -0500

    Add Oil plugin.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -41,26 +41,7 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 vim.keymap.set('n', '<leader>c', '<C-w>q', { desc = 'Quit window' })
 vim.keymap.set('n', '<leader>w', '<C-w>', { desc = 'windows' })
 
-vim.keymap.set('n', '-', function()
-  local reveal_file = vim.fn.expand '%:p'
-  if reveal_file == '' then
-    reveal_file = vim.fn.getcwd()
-  else
-    local f = io.open(reveal_file, 'r')
-    if f then
-      f.close(f)
-    else
-      reveal_file = vim.fn.getcwd()
-    end
-  end
-  require('neo-tree.command').execute {
-    action = 'focus', -- OPTIONAL, this is the default value
-    source = 'filesystem', -- OPTIONAL, this is the default value
-    position = 'left', -- OPTIONAL, this is the default value
-    reveal_file = reveal_file, -- path to file or folder to reveal
-    reveal_force_cwd = true, -- change cwd without asking if needed
-  }
-end, { desc = 'Open Neotree at current file or working directory' })
+vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
 
 -- Little terminal
 local job_id = 0
