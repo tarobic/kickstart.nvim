51c06685dc76b9ce682950e1ff753c87cff83acf
tarobic <msullentrup1@gmail.com>
2025-05-16 15:13:39 -0500

    Change some options to vim.o

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,46 +1,46 @@
 vim.g.have_nerd_font = true
 
-vim.opt.number = true
-vim.opt.mouse = "a"
-vim.opt.undofile = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.signcolumn = "yes"
-vim.opt.updatetime = 250
+vim.o.number = true
+vim.o.mouse = "a"
+vim.o.undofile = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.signcolumn = "yes"
+vim.o.updatetime = 250
 
--- Wrap options
-vim.opt.linebreak = true
-vim.opt.breakindent = true
-vim.opt.showbreak = "+++ "
+-- Wrap oions
+vim.o.linebreak = true
+vim.o.breakindent = true
+vim.o.showbreak = "+++ "
 
 -- This affects both:
 -- How long to wait between leader-w to show which-key menu.
 -- How quickly to press leader-e to toggle neotree while neotree is focused.
-vim.opt.timeoutlen = 200
-vim.opt.inccommand = "split"
-vim.opt.cursorline = true
-vim.opt.scrolloff = 10
-vim.opt.confirm = true
-vim.opt.showmode = false
-vim.opt.winborder = "rounded"
-vim.opt.mousefocus = true
+vim.o.timeoutlen = 200
+vim.o.inccommand = "split"
+vim.o.cursorline = true
+vim.o.scrolloff = 10
+vim.o.confirm = true
+vim.o.showmode = false
+vim.o.winborder = "rounded"
+vim.o.mousefocus = true
 
 -- Configure how new splits should be opened
-vim.opt.splitright = true
-vim.opt.splitbelow = true
+vim.o.splitright = true
+vim.o.splitbelow = true
 
 -- Folds
-vim.opt.foldmethod = "indent"
-vim.opt.foldcolumn = "3"
+vim.o.foldmethod = "indent"
+vim.o.foldcolumn = "3"
 
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
-vim.opt.list = true
+vim.o.list = true
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
 vim.schedule(function()
-   vim.opt.clipboard = "unnamedplus"
+   vim.o.clipboard = "unnamedplus"
 end)
 
 vim.cmd "set noexpandtab"
@@ -57,12 +57,12 @@ vim.cmd "set nocindent"
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
+--  See `:help vim.hl.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
    desc = "Highlight when yanking (copying) text",
    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
    callback = function()
-      vim.highlight.on_yank()
+      vim.hl.on_yank()
    end,
 })
 
