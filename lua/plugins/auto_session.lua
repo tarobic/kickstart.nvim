2e7a88d0d86b7a5dd59b7305781804e5f10f381d
tarobic <msullentrup1@gmail.com>
2025-05-22 13:26:30 -0500

    Add auto session plugin

---
+++ lua/plugins/auto_session.lua
@@ -0,0 +1,13 @@
+return {
+   "rmagatti/auto-session",
+   enabled = false,
+   lazy = false,
+
+   ---enables autocomplete for opts
+   ---@module "auto-session"
+   ---@type AutoSession.Config
+   opts = {
+      suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+      -- log_level = 'debug',
+   },
+}
