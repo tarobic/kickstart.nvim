b700f459300a60bc37195d8bd9fa0df36daff1f3
tarobic <msullentrup1@gmail.com>
2025-05-28 10:35:36 -0500

    Change to mini.snippets

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,73 +1,30 @@
 return { -- Autocompletion
-   "saghen/blink.cmp",
-   enabled = false,
-   event = "VimEnter",
-   version = "1.*",
-   dependencies = {
-      -- Snippet Engine
-      {
-         "L3MON4D3/LuaSnip",
-         version = "2.*",
-         build = (function()
-            -- Build Step is needed for regex support in snippets.
-            -- This step is not supported in many windows environments.
-            -- Remove the below condition to re-enable on windows.
-            if vim.fn.has "win32" == 1 or vim.fn.executable "make" == 0 then
-               return
-            end
-            return "make install_jsregexp"
-         end)(),
-         dependencies = {
-            --    https://github.com/rafamadriz/friendly-snippets
-            {
-               "rafamadriz/friendly-snippets",
-               config = function()
-                  require("luasnip.loaders.from_vscode").lazy_load()
-               end,
-            },
-         },
-         opts = {},
-      },
-      "folke/lazydev.nvim",
-   },
-   --- @module 'blink.cmp'
-   --- @type blink.cmp.Config
-   opts = {
-      keymap = {
-         -- All presets have the following mappings:
-         -- <tab>/<s-tab>: move to right/left of your snippet expansion
-         -- <c-space>: Open menu or open docs if already open
-         -- <c-n>/<c-p> or <up>/<down>: Select next/previous item
-         -- <c-e>: Hide menu
-         -- <c-k>: Toggle signature help
-         --
-         -- See :h blink-cmp-config-keymap for defining your own keymap
-         preset = "default",
+    "saghen/blink.cmp",
+    -- enabled = false,
+    event = "VimEnter",
+    version = "1.*",
+    dependencies = {
+        { "echasnovski/mini.snippets" },
+        -- "folke/lazydev.nvim",
+    },
+    --- @module 'blink.cmp'
+    --- @type blink.cmp.Config
+    opts = {
+        appearance = { nerd_font_variant = "mono", },
+        completion = {
+            documentation = { auto_show = true, auto_show_delay_ms = 500 },
+        },
+        sources = {
+            -- default = { "lsp", "path", "snippets", "lazydev" },
+            -- providers = {
+            --     lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
+            -- },
+            default = { "lsp", "path", "snippets", "buffer" },
+        },
+        snippets = { preset = "mini_snippets" },
+        fuzzy = { implementation = "prefer_rust_with_warning" },
 
-         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
-      },
-
-      appearance = {
-         nerd_font_variant = "mono",
-      },
-
-      completion = {
-         -- By default, you may press `<c-space>` to show the documentation.
-         -- Optionally, set `auto_show = true` to show the documentation after a delay.
-         documentation = { auto_show = false, auto_show_delay_ms = 500 },
-      },
-
-      sources = {
-         default = { "lsp", "path", "snippets", "lazydev" },
-         providers = {
-            lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
-         },
-      },
-      snippets = { preset = "luasnip" },
-      fuzzy = { implementation = "prefer_rust_with_warning" },
-      --
-      -- Shows a signature help window while you type arguments for a function
-      signature = { enabled = true },
-   },
+        -- Shows a signature help window while you type arguments for a function
+        signature = { enabled = true },
+    },
 }
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,39 +1,40 @@
 local function checkIfDefault(name)
-   -- if DefaultColorscheme == name then
-   --    return true, 1000
-   -- end
-   -- return false, 0
-   return DefaultColorscheme == name
+    -- if DefaultColorscheme == name then
+    --    return true, 1000
+    -- end
+    -- return false, 0
+    return DefaultColorscheme == name
 end
 
 return {
-   {
-      "catppuccin/nvim",
-      -- lazy, priority = checkIfDefault("catppuccin"),
-      lazy = not checkIfDefault("catppuccin"),
-   },
-   {
-      "rebelot/kanagawa.nvim",
-      -- lazy, priority = checkIfDefault("kanagawa"),
-      lazy = not checkIfDefault("kanagawa")
-   },
-   -- Treesitter combatible:
-   {
-      "folke/tokyonight.nvim",
-      -- lazy, priority = checkIfDefault("tokyonight"),
-      lazy = not checkIfDefault("tokyonight")
-      -- config = function()
-      --   ---@diagnostic disable-next-line: missing-fields
-      --   require('tokyonight').setup {
-      --     styles = {
-      --       comments = { italic = false }, -- Disable italics in comments
-      --     },
-      --   }
-      -- end,
-   },
-   {
-      "Mofiqul/dracula.nvim",
-      -- lazy, priority = checkIfDefault("dracula"),
-      lazy = not checkIfDefault("dracula")
-   },
+    {
+        "catppuccin/nvim",
+        -- lazy, priority = checkIfDefault("catppuccin"),
+        lazy = not checkIfDefault("catppuccin"),
+    },
+    {
+        "rebelot/kanagawa.nvim",
+        -- lazy, priority = checkIfDefault("kanagawa"),
+        lazy = not checkIfDefault("kanagawa")
+    },
+    -- Treesitter combatible:
+    {
+        "folke/tokyonight.nvim",
+        -- lazy, priority = checkIfDefault("tokyonight"),
+        lazy = not checkIfDefault("tokyonight")
+        -- config = function()
+        --   ---@diagnostic disable-next-line: missing-fields
+        --   require('tokyonight').setup {
+        --     styles = {
+        --       comments = { italic = false }, -- Disable italics in comments
+        --     },
+        --   }
+        -- end,
+    },
+    {
+        "Mofiqul/dracula.nvim",
+        -- lazy, priority = checkIfDefault("dracula"),
+        lazy = not checkIfDefault("dracula")
+    },
+    { "vague2k/vague.nvim", lazy = not checkIfDefault("vague") }
 }
