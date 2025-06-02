e98fc2d359fe0df2beb079d350fe9addefd39e08
tarobic <msullentrup1@gmail.com>
2025-06-02 15:54:37 -0500

    Add todo-comments.nvim keymaps for integration with snacks.picker

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,7 +1,20 @@
--- Highlight todo, notes, etc in comments
 return {
 	"folke/todo-comments.nvim",
-	event = "VimEnter",
+	-- event = "VimEnter",
 	dependencies = { "nvim-lua/plenary.nvim" },
-	opts = { signs = false },
+	opts = {},
+	keys = {
+		{
+			"<leader>st",
+			function() Snacks.picker.todo_comments() end,
+			desc = "Todo",
+		},
+		{
+			"<leader>sT",
+			function()
+				Snacks.picker.todo_comments { keywords = { "TODO", "FIX", "FIXME" } }
+			end,
+			desc = "Todo/Fix/Fixme",
+		},
+	},
 }
