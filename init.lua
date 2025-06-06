72e705daa119445f7403f86998bb8b9d82320767
tarobic <msullentrup1@gmail.com>
2025-06-06 16:52:17 -0500

    Add floating terminal

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ require "core.lsp"
 require "core.lazy"
 require "config.options"
 require "config.autocommands"
+require "floaterminal"
 
 -- transparent background
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
