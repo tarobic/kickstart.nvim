acef207dbae9f41937961b462c976c491765fb87
tarobic <msullentrup1@gmail.com>
2025-06-05 19:12:07 -0500

    Work on autorun command

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -165,3 +165,43 @@ vim.api.nvim_create_autocmd("VimEnter", {
 vim.api.nvim_create_autocmd("ColorScheme", {
 	callback = function(params) vim.g.SCHEME = params.match end,
 })
+
+--FIXME
+-- Auto run file when saved
+local attach_to_buffer = function(output_bufnr, pattern, command)
+	vim.api.nvim_create_autocmd("BufWritePost", {
+		group = vim.api.nvim_create_augroup("RunOnSave", { clear = true }),
+		pattern = pattern,
+		callback = function()
+			local append_data = function(_, data)
+				if data then
+					vim.api.nvim_buf_set_lines(output_bufnr, -1, -1, false, data)
+				end
+			end
+			vim.api.nvim_buf_set_lines(
+				output_bufnr,
+				0,
+				-1,
+				false,
+				{ "output of: lua file" }
+			)
+			vim.fn.jobstart(command, {
+				stdout_buffered = true,
+				on_stdout = append_data,
+				on_stderr = append_data,
+			})
+		end,
+	})
+end
+
+vim.api.nvim_create_user_command("AutoRun", function()
+	vim.cmd.vnew()
+	vim.cmd.term()
+	local bufnr = vim.bo.channel
+	print "Autorunning..."
+	-- local bufnr = vim.fn.input "Bufnr: "
+	local pattern = vim.fn.input "Pattern: "
+	-- local pattern = "*.lua"
+	local command = vim.split(vim.fn.input "Command: ", " ")
+	attach_to_buffer(tonumber(bufnr), pattern, command)
+end, {})
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,20 +11,7 @@ vim.keymap.set(
 	{ desc = "Quickfix list" }
 )
 
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
--- vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 vim.keymap.set("t", "<C-[>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
-
--- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
 vim.keymap.set(
 	"n",
@@ -51,47 +38,30 @@ vim.keymap.set(
 	{ desc = "Move focus to the upper window" }
 )
 
--- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
--- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
--- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
--- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
--- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
-
 vim.keymap.set("n", "<leader>w", "<C-w>", { desc = "Windows" })
 
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
--- Little terminal
-local job_id = 0
-vim.keymap.set("n", "<leader>lt", function()
+local function openLittleTerminal()
 	vim.cmd.vnew()
 	vim.cmd.term()
 	vim.cmd.wincmd "J"
 	vim.api.nvim_win_set_height(0, 15)
 
-	job_id = vim.bo.channel
-end, { desc = "Little Terminal" })
+	return vim.bo.channel
+end
 
 -- Run a custom command in little terminal
 vim.keymap.set("n", "<leader>lc", function()
 	-- Replace 'ls -al' with make, go build, etc.
+	local job_id = openLittleTerminal()
 	vim.fn.chansend(job_id, { "ls -al\r\n" })
 end, { desc = "Terminal command" })
 
--- vim.keymap.set({ "n", "v" }, "<leader>dh", function()
---    require("dap.ui.widgets").hover()
--- end)
--- vim.keymap.set({ "n", "v" }, "<Leader>dp", function()
---    require("dap.ui.widgets").preview()
--- end)
--- vim.keymap.set("n", "<Leader>df", function()
---    local widgets = require "dap.ui.widgets"
---    widgets.centered_float(widgets.frames)
--- end)
--- vim.keymap.set("n", "<Leader>ds", function()
---    local widgets = require "dap.ui.widgets"
---    widgets.centered_float(widgets.scopes)
--- end)
+vim.keymap.set("n", "<leader>ll", function()
+	local job_id = openLittleTerminal()
+	vim.fn.chansend(job_id, { "lua " .. vim.fn.expand "%" })
+end, { desc = "Run current lua file" })
 
 vim.keymap.set("n", "<leader>bd", function()
 	for _, buffer in pairs(vim.fn.getbufinfo()) do
@@ -104,7 +74,7 @@ vim.keymap.set("n", "gk", "<Up>")
 vim.keymap.set("n", "gj", "<Down>")
 
 -- Source current file.
--- vim.keymap.set("n", "<space>x", "<cmd>source % <CR>")
+vim.keymap.set("n", "<leader>rx", "<cmd>source % <CR>")
 -- Execute current line.
 -- vim.keymap.set("n", "<space>X", ":.lua<CR>")
 -- Execute selected lines.
@@ -135,3 +105,5 @@ vim.keymap.set(
 	function() require("lazy").home() end,
 	{ desc = "Lazy" }
 )
+
+-- vim.keymap.set("n", ":", "<cmd>FineCmdline<CR>")
