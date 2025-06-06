a461a4fe211a0b505a1079720c5010240cf643ec
tarobic <msullentrup1@gmail.com>
2025-06-06 13:30:27 -0500

    Enable some mini modules

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -2,10 +2,13 @@ return { -- Collection of various small independent plugins/modules
 	"echasnovski/mini.nvim",
 	version = false,
 	config = function()
-		require("mini.ai").setup { n_lines = 500 }
+		require("mini.ai").setup()
+		require("mini.comment").setup()
 		require("mini.diff").setup()
 		require("mini.git").setup()
 		require("mini.icons").setup()
+		require("mini.move").setup()
+		require("mini.operators").setup()
 		require("mini.pairs").setup {
 			opts = {
 				modes = { insert = true, command = true, terminal = false },
