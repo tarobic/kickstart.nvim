b4c774b41d63b892d5cd062ec24fa88a4271b3f9
tarobic <msullentrup1@gmail.com>
2025-07-17 18:26:58 -0500

    Replace debug config with easy-dotnet example

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -1,148 +1,133 @@
--- debug.lua
---
--- Shows how to use the DAP plugin to debug your code.
---
--- Primarily focused on configuring the debugger for Go, but can
--- be extended to other languages as well. That's why it's called
--- kickstart.nvim and not kitchen-sink.nvim ;)
+local function rebuild_project(co, path)
+	local spinner = require("easy-dotnet.ui-modules.spinner").new()
+	spinner:start_spinner "Building"
+	vim.fn.jobstart(string.format("dotnet build %s", path), {
+		on_exit = function(_, return_code)
+			if return_code == 0 then
+				spinner:stop_spinner "Built successfully"
+			else
+				spinner:stop_spinner(
+					"Build failed with exit code " .. return_code,
+					vim.log.levels.ERROR
+				)
+				error "Build failed"
+			end
+			coroutine.resume(co)
+		end,
+	})
+	coroutine.yield()
+end
 
 return {
-  -- NOTE: Yes, you can install new plugins here!
-  'mfussenegger/nvim-dap',
-  -- NOTE: And you can specify dependencies as well
-  dependencies = {
-    -- Creates a beautiful debugger UI
-    'rcarriga/nvim-dap-ui',
+	"mfussenegger/nvim-dap",
+	enabled = true,
+	dependencies = {
+		-- Creates a beautiful debugger UI
+		"rcarriga/nvim-dap-ui",
 
-    -- Required dependency for nvim-dap-ui
-    'nvim-neotest/nvim-nio',
+		-- Required dependency for nvim-dap-ui
+		"nvim-neotest/nvim-nio",
 
-    -- Installs the debug adapters for you
-    'williamboman/mason.nvim',
-    'jay-babu/mason-nvim-dap.nvim',
+		-- Installs the debug adapters for you
+		"williamboman/mason.nvim",
+		"jay-babu/mason-nvim-dap.nvim",
 
-    -- Add your own debuggers here
-    'leoluz/nvim-dap-go',
-  },
-  keys = {
-    -- Basic debugging keymaps, feel free to change to your liking!
-    {
-      '<F5>',
-      function()
-        require('dap').continue()
-      end,
-      desc = 'Debug: Start/Continue',
-    },
-    {
-      '<F1>',
-      function()
-        require('dap').step_into()
-      end,
-      desc = 'Debug: Step Into',
-    },
-    {
-      '<F2>',
-      function()
-        require('dap').step_over()
-      end,
-      desc = 'Debug: Step Over',
-    },
-    {
-      '<F3>',
-      function()
-        require('dap').step_out()
-      end,
-      desc = 'Debug: Step Out',
-    },
-    {
-      '<leader>b',
-      function()
-        require('dap').toggle_breakpoint()
-      end,
-      desc = 'Debug: Toggle Breakpoint',
-    },
-    {
-      '<leader>B',
-      function()
-        require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ')
-      end,
-      desc = 'Debug: Set Breakpoint',
-    },
-    -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-    {
-      '<F7>',
-      function()
-        require('dapui').toggle()
-      end,
-      desc = 'Debug: See last session result.',
-    },
-  },
-  config = function()
-    local dap = require 'dap'
-    local dapui = require 'dapui'
+		-- Add your own debuggers here
+		"leoluz/nvim-dap-go",
+	},
+	config = function()
+		local dap = require "dap"
+		local dotnet = require "easy-dotnet"
+		local dapui = require "dapui"
+		dap.set_log_level "TRACE"
 
-    require('mason-nvim-dap').setup {
-      -- Makes a best effort to setup the various debuggers with
-      -- reasonable debug configurations
-      automatic_installation = true,
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
 
-      -- You can provide additional configuration to the handlers,
-      -- see mason-nvim-dap README for more information
-      handlers = {},
+		vim.keymap.set("n", "q", function()
+			dap.close()
+			dapui.close()
+		end, {})
 
-      -- You'll need to check that you have the required things installed
-      -- online, please don't ask me how to install them :)
-      ensure_installed = {
-        -- Update this to ensure that you have the debuggers for the langs you want
-        'delve',
-      },
-    }
+		vim.keymap.set("n", "<F5>", dap.continue, {})
+		vim.keymap.set("n", "<F10>", dap.step_over, {})
+		vim.keymap.set("n", "<leader>dO", dap.step_over, {})
+		vim.keymap.set("n", "<leader>dC", dap.run_to_cursor, {})
+		vim.keymap.set("n", "<leader>dr", dap.repl.toggle, {})
+		vim.keymap.set("n", "<leader>dj", dap.down, {})
+		vim.keymap.set("n", "<leader>dk", dap.up, {})
+		vim.keymap.set("n", "<F11>", dap.step_into, {})
+		vim.keymap.set("n", "<F12>", dap.step_out, {})
+		vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, {})
+		vim.keymap.set("n", "<F2>", require("dap.ui.widgets").hover, {})
 
-    -- Dap UI setup
-    -- For more information, see |:help nvim-dap-ui|
-    dapui.setup {
-      -- Set icons to characters that are more likely to work in every terminal.
-      --    Feel free to remove or use ones that you like more! :)
-      --    Don't feel like these are good choices.
-      icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
-      controls = {
-        icons = {
-          pause = '⏸',
-          play = '▶',
-          step_into = '⏎',
-          step_over = '⏭',
-          step_out = '⏮',
-          step_back = 'b',
-          run_last = '▶▶',
-          terminate = '⏹',
-          disconnect = '⏏',
-        },
-      },
-    }
+		local function file_exists(path)
+			local stat = vim.loop.fs_stat(path)
+			return stat and stat.type == "file"
+		end
 
-    -- Change breakpoint icons
-    -- vim.api.nvim_set_hl(0, 'DapBreak', { fg = '#e51400' })
-    -- vim.api.nvim_set_hl(0, 'DapStop', { fg = '#ffcc00' })
-    -- local breakpoint_icons = vim.g.have_nerd_font
-    --     and { Breakpoint = '', BreakpointCondition = '', BreakpointRejected = '', LogPoint = '', Stopped = '' }
-    --   or { Breakpoint = '●', BreakpointCondition = '⊜', BreakpointRejected = '⊘', LogPoint = '◆', Stopped = '⭔' }
-    -- for type, icon in pairs(breakpoint_icons) do
-    --   local tp = 'Dap' .. type
-    --   local hl = (type == 'Stopped') and 'DapStop' or 'DapBreak'
-    --   vim.fn.sign_define(tp, { text = icon, texthl = hl, numhl = hl })
-    -- end
+		local debug_dll = nil
 
-    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
-    dap.listeners.before.event_exited['dapui_config'] = dapui.close
+		local function ensure_dll()
+			if debug_dll ~= nil then
+				return debug_dll
+			end
+			local dll = dotnet.get_debug_dll()
+			debug_dll = dll
+			return dll
+		end
 
-    -- Install golang specific config
-    require('dap-go').setup {
-      delve = {
-        -- On Windows delve must be run attached or it crashes.
-        -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
-        detached = vim.fn.has 'win32' == 0,
-      },
-    }
-  end,
+		for _, value in ipairs { "cs", "fsharp" } do
+			dap.configurations[value] = {
+				{
+					type = "coreclr",
+					name = "Program",
+					request = "launch",
+					env = function()
+						local dll = ensure_dll()
+						local vars = dotnet.get_environment_variables(
+							dll.project_name,
+							dll.absolute_project_path
+						)
+						return vars or nil
+					end,
+					program = function()
+						local dll = ensure_dll()
+						local co = coroutine.running()
+						rebuild_project(co, dll.project_path)
+						if not file_exists(dll.target_path) then
+							error(
+								"Project has not been built, path: " .. dll.target_path
+							)
+						end
+						return dll.target_path
+					end,
+					cwd = function()
+						local dll = ensure_dll()
+						return dll.absolute_project_path
+					end,
+				},
+			}
+
+			dap.listeners.before["event_terminated"]["easy-dotnet"] = function()
+				debug_dll = nil
+			end
+
+			dap.adapters.coreclr = {
+				type = "executable",
+				command = "netcoredbg",
+				args = { "--interpreter=vscode" },
+			}
+		end
+	end,
 }
