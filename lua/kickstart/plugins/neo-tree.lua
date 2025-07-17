d7236df6f3f4119b3a45bc1c89c4978ebea6e60e
tarobic <msullentrup1@gmail.com>
2025-07-17 18:27:26 -0500

    Add easy-dotnet neo-tree configuration

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -2,24 +2,35 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
-  },
-  lazy = false,
-  keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
-  },
-  opts = {
-    filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-        },
-      },
-    },
-  },
+	"nvim-neo-tree/neo-tree.nvim",
+	version = "*",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	lazy = false,
+	keys = {
+		{ "\\", ":Neotree reveal<CR>", desc = "NeoTree reveal", silent = true },
+	},
+	opts = {
+		filesystem = {
+			window = {
+				mappings = {
+					["\\"] = "close_window",
+					["R"] = "easy",
+				},
+			},
+		},
+		commands = {
+			["easy"] = function(state)
+				local node = state.tree:get_node()
+				local path = node.type == "directory" and node.path
+					or vim.fs.dirname(node.path)
+				require("easy-dotnet").create_new_item(path, function()
+					require("neo-tree.sources.manager").refresh(state.name)
+				end)
+			end,
+		},
+	},
 }
